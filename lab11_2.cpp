22fed7e3f9f12a724a0f5bd8630824d32226e7c0
tigerlab <tiggers12@hotmail.com>
2023-01-03 15:45:36 +0700

    stt

--- lab11_2.cpp
+++ lab11_2.cpp
@@ -2,15 +2,20 @@
 #include<fstream>
 #include<string>
 using namespace std;
-
 int main (){
 	ifstream source;
 	ofstream dest;
 	source.open("cheerbook.txt") ;
 	dest.open("cheerbook_copy.txt");
 	
-	//กรอก Code เฉพาะส่วนนี้ ให้สอดคล้องกับโจทย์และ Code ส่วนอื่น ๆ 
-	//โดยห้ามแก้ไข Code ในส่วนอื่น ๆ
+	dest << "-------------------- BOOM ---------------------" << endl;
+	string textline;
+	for (int i =0 ; i<9 ; i++)
+	{
+	getline(source,textline);
+    dest << textline << endl;
+	}
+	dest << "-------------------- HA!! ---------------------" << endl;
 
     source.close();
     dest.close();
